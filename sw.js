bf91a901937345872c47b4d973ec3e291dac8f96
connectsilva <connectsilvaoficial@gmail.com>
2026-05-07 17:37:59 -0300

    Teste

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'batismo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'
+];
+
+// Instalação e Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Resposta offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
